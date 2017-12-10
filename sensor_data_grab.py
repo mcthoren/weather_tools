40e0f93f300e10f4a3b9a8a9ada705a48196e7e6
mcthoren <thoren.mcdole@gmail.com>
2017-12-10 20:59:03 +0000

    begin branch for new gen weather probe, simpler, cheaper, smaller

--- sensor_data_grab.py
+++ sensor_data_grab.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python
 
-# written with help from:
-# https://github.com/adafruit/Adafruit_HTU21DF_Library/
-# https://github.com/adafruit/Adafruit_Python_BMP/
-# https://github.com/dalexgray/RaspberryPI_HTU21DF/
 
 import time
 import datetime
+import bme680
 
 wx_dir = "/home/ghz/wx"
 
@@ -19,105 +16,30 @@ def write_out_dat_stamp(ts, n_plate, data):
 	f_ts = ts[0:8]
 	write_out(wx_dir+'/data/'+n_plate+'.'+f_ts, data, 'a')
 
-def htu21df_read():
-	# https://www.adafruit.com/product/1899
-	
-	import Adafruit_GPIO.I2C as I2C
-	import math
+def bme680_read():
+	sensor = bme680.BME680(i2c_addr=0x77)
 
-	temp_read = 0xE3
-	hum_read = 0xE5
-	write_reg = 0xE6
-	soft_reset = 0xFE
+	sensor.set_humidity_oversample(bme680.OS_2X)
+	sensor.set_pressure_oversample(bme680.OS_4X)
+	sensor.set_temperature_oversample(bme680.OS_8X)
+	sensor.set_filter(bme680.FILTER_SIZE_3)
+	sensor.set_gas_status(bme680.ENABLE_GAS_MEAS)
 
-	# constants from the datasheet
-	const_a = 8.1332
-	const_b = 1762.39
-	const_c = 235.66
-
-	htu_addy = 0x40
-	bus = 0
-
-	array = [0x00, 0x00]
-
-	dev0 = I2C.Device(htu_addy, bus)
-	dev0.write8(soft_reset, write_reg) # does this actually reset it? prly not....
-	time.sleep(0.2)
-
-	ts =  datetime.datetime.fromtimestamp(time.time()).strftime("%Y%m%d%H%M%S")
-
-	array = dev0.readList(temp_read, 2)
-	t0 = (array[0] * 256.0) + array[1]
-	temp = ((t0 * 175.72) / 65536) - 46.85
-
-	array = dev0.readList(hum_read, 2)
-	h0 = (array[0] * 256.0) + array[1]
-	hum = ((h0 * 125) / 65536) - 6
-
-	P_part = 10 ** (const_a - (const_b / (temp + const_c)))
-	T_dew = -1 * ((const_b / (math.log10(hum * (P_part / 100)) - const_a)) + const_c)
-
-	dat_string = "%s\tTemp: %.2f C\tHumidity: %.2f %%\tDew Point: %.2f C\n" % (ts, temp, hum, T_dew)
-
-	write_out_dat_stamp(ts, 'htu21df_grab.dat', dat_string)
-	
-	return (temp, hum, T_dew)
-
-def bmp085_read():
-	# https://www.adafruit.com/products/391
-	import Adafruit_BMP.BMP085 as BMP085
-
-	pressures = []
-	iter = 16       
-	avg = 0
-
-	sensor = BMP085.BMP085(mode=BMP085.BMP085_ULTRAHIGHRES)
+	sensor.set_gas_heater_temperature(320)
+	sensor.set_gas_heater_duration(150)
+	sensor.select_gas_heater_profile(0)
 
 	ts = datetime.datetime.fromtimestamp(time.time()).strftime("%Y%m%d%H%M%S")
 
-	temp = sensor.read_temperature()
+	temp = sensor.data.temperature
+	pressure = sensor.data.pressure
+	hum = sensor.data.humidity
 
-	# datasheet suggests averaging
-	for x in range(0, iter):
-		pressures.append(sensor.read_pressure())
-		avg += pressures[x]
+	gas_res = sensor.data.gas_resistance
 
-	avg = avg / iter
-
-	# means sea level pressure calibration
-	# calibrated against:
-	# http://www.geomar.de/service/wetter/
-	# [kPa]
-	mslp_calibration = .749
-
-	dat_string = "%s\t%.2f C\t\t%.3f kPa\n" % (ts, temp, ((avg / 1000.0) + mslp_calibration))
-
-	write_out_dat_stamp(ts, 'bmp0085_grab.dat', dat_string)
-
-	return (temp, (avg / 1000.0) + mslp_calibration)
-
-def sht11_read():
-	# basically the sample code from the docs:
-	# https://pypi.python.org/pypi/rpiSht1x/1.2
-
-	from sht1x.Sht1x import Sht1x as SHT1x
-	dataPin = 16
-	clkPin = 7
-
-	sht1x = SHT1x(dataPin, clkPin, SHT1x.GPIO_BOARD)
-
-	ts = datetime.datetime.fromtimestamp(time.time()).strftime("%Y%m%d%H%M%S")
-
-	temperature = sht1x.read_temperature_C()
-	humidity = sht1x.read_humidity()
-	dp = sht1x.calculate_dew_point(temperature, humidity)
-
-	dat_string = "%s\tTemp: %.2f C\tHumidity: %.2f %%\tDew Point: %.2f C\n" % (ts, temperature, humidity, dp)
-
-	write_out_dat_stamp(ts, 'sht11_grab.dat', dat_string)
-
-	return (temperature, humidity, dp)
-
+	dat_string = "%s\tTemp: %.2f C\tHumidity: %.2f %%\tPressure: %.2f kPa\tAirQ: %.0f Ohms\n" % (ts, temp, hum, pressure / 10, gas_res)
+	print dat_string
+		
 def pi_temp_read():
 	temp_file = "/sys/class/thermal/thermal_zone0/temp"
 	temp_file_fd = open(temp_file, 'r')
@@ -154,8 +76,6 @@ def gen_index(etemp, ehum, edp, press, bmptemp, itemp, ihum, idp, pitemp):
 	write_out(wx_dir+'/plots/wx.html', plate_dat, 'w')
 
 if __name__ == "__main__":
-	(bmp_temp, press) = bmp085_read()
-	(e_temp, e_hum, e_dp) = htu21df_read()
-	(i_temp, i_hum, i_dp) = sht11_read()
-	pi_temp = pi_temp_read()
-	gen_index(e_temp, e_hum, e_dp, press, bmp_temp, i_temp, i_hum, i_dp, pi_temp)
+	# pi_temp = pi_temp_read()
+	# gen_index(e_temp, e_hum, e_dp, press, bmp_temp, i_temp, i_hum, i_dp, pi_temp)
+	bme680_read()
