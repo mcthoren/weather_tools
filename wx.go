8f5d07299b5fd9f4e9d82ba19fbbf2e6cb0baf01
mcthoren <thoren.mcdole@gmail.com>
2019-06-29 14:22:01 +0000

    plumb in shell side of derived data graphing

--- wx.go
+++ wx.go
@@ -22,6 +22,7 @@ $OVER_DIR/sensor_data_grab.py
 cd $BASE_DIR/ ||exit 1
 TD="../data/last48h"
 TDPT="../data/last48h.pitemp"
+TDDD="../data/last48h.derived"
 
 DY=`date +%Y`
 YDY=`date -d "-1 day" +%Y`
@@ -42,19 +43,21 @@ PAT1="^$2([0-9]{14})\t-?[0-9]{5}$"
 
 TDS0="cat ../data/$YYDY/bme680.dat.$YYDATE ../data/$YDY/bme680.dat.$YDATE ../data/$DY/bme680.dat.$DATE | grep -aP \"$PAT0\""
 TDS1="cat ../data/$YYDY/pi_temp.$YYDATE ../data/$YDY/pi_temp.$YDATE ../data/$DY/pi_temp.$DATE | grep -aP \"$PAT1\""
+TDS2="cat ../data/$YYDY/derived.dat.$YYDATE ../data/$YDY/derived.dat.$YDATE ../data/$DY/derived.dat.$DATE"
 
 TD_DUMP0="eval $TDS0"
 TD_DUMP1="eval $TDS1"
+TD_DUMP2="eval $TDS2"
 
 $TD_DUMP0 | grep -A $NSAMP $YYDATEH > $TD || $TD_DUMP0 > $TD
 $TD_DUMP1 | grep -A $NSAMP $YYDATEH > $TDPT || $TD_DUMP1 > $TDPT
+$TD_DUMP2 | grep -A $NSAMP $YYDATEH > $TDDD || $TD_DUMP2 > $TDDD
 
-gnuplot -e "TD='$TD';TDPT='$TDPT'" $OVER_DIR/weather_specs.gnuplot
+gnuplot -e "TD='$TD';TDPT='$TDPT';TDDD='$TDDD'" $OVER_DIR/weather_specs.gnuplot
 
 # try not to loose 20 min of data from hard reboots
 sync
 
-# rsync -e "ssh -q" --timeout=60 -ur $OVER_DIR/* wx1@darkdata.org:/wx1/ 2>/dev/null
 rsync -e "ssh -q" --timeout=60 -ur $OVER_DIR/* wx1@dunkledaten.de:/wx1/ 2>/dev/null
 
 rm $LOCK
