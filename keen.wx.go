4beb1c59dbbfbadcff1aafb0a19b945e359e4b82
mcthoren <thoren.mcdole@gmail.com>
2021-04-20 18:51:47 +0000

    add locking and syncing for us and dust

--- keen.wx.go
+++ keen.wx.go
@@ -2,6 +2,16 @@
 
 # meant to be called from cron every minute or so
 
+LOCK="/home/ghz/wx/wx.lock"
+
+[ -e "${LOCK}" ] && {
+	echo "$0: lock exists" | logger
+	exit 1
+}
+
+# lock is also checked for and deleted on boot, in case of a crash
+touch "${LOCK}"
+
 WT_DIR='/import/home/ghz/repos/weather_tools/'
 
 $WT_DIR/sensor_data_grab.py
@@ -14,3 +24,7 @@ cd /home/ghz/wx/plots || exit 1
 gnuplot "$WT_DIR/keen.wx.gnuplot"
 
 sync
+
+/usr/bin/rsync -ur --timeout=50 /home/ghz/wx /import/home/ghz/repos/dust_wx wx0_sync:/wx0/
+
+rm "${LOCK}"
