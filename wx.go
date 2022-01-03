75bff880e68fd06a649a3922eaf81a54d9a6bf9e
mcthoren <thoren.mcdole@gmail.com>
2022-01-03 19:12:07 +0000

    first step of simplifying and unifiying different weather grabby scripts.

--- wx.go
+++ wx.go
@@ -1,13 +1,9 @@
 #!/bin/bash
-# see http://slackology.net/wxnotes.html and
-# https://github.com/mcthoren/weather_tools for more info
-# this is about the 4th generation weather station at this point
+
 # this script is run from cron
 
-OVER_DIR="/home/ghz/wx"
-LOCK="$OVER_DIR/plots/LOCK.wx"
-BASE_DIR="$OVER_DIR/plots"
-NSAMP=4000
+WT_DIR="/home/ghz/wx"
+LOCK="$WT_DIR/plots/LOCK.wx"
 
 [ -e $LOCK ] && {
 	echo "$0: lock exists" | logger
@@ -17,44 +13,17 @@ NSAMP=4000
 # lock is also checked for and deleted on boot, in case of a crash
 touch $LOCK
 
-$OVER_DIR/sensor_data_grab.py
+$WT_DIR/sensor_data_grab.py
 
-cd $BASE_DIR/ ||exit 1
-TD="../data/last48h"
-TDPT="../data/last48h.pitemp"
-TDDD="../data/last48h.derived"
+$WT_DIR/grab_48h /home/ghz/wx/data bme680.dat
+$WT_DIR/grab_48h /home/ghz/wx/data derived.dat
+$WT_DIR/grab_48h /home/ghz/wx/data pi_temp
 
-DY=`date +%Y`
-YDY=`date -d "-1 day" +%Y`
-YYDY=`date -d "-2 day" +%Y`
-DATE=`date +%Y%m%d`
-YDATE=`date -d "-1 day" +%Y%m%d`
-YYDATE=`date -d "-2 day" +%Y%m%d`
-YYDATEH=`date -d "-2 day" +%Y%m%d%H`
+cd /home/ghz/wx/plots || exit 1
+gnuplot $WT_DIR/cutie.wx.gnuplot
 
-# make sure our data is sth like what we expect, this has evolved from necessity.
-PAT0="^2([0-9]{13})\tTemp:\ -?[0-9]?[0-9]\.[0-9]{2} C\tHumidity:\ [0-9]?[0-9]?[0-9]\.[0-9]{2} %\tPressure:\ [0-9]*\.[0-9]{3} kPa\tAirQ:\ .*Ohms$"
-
-# 4 digits for when it gets more properly cold, 5 digits otherwise. 6 digits have not been seen.
-PAT1="^$2([0-9]{14})\t-?[0-9]{4,5}$"
-
-TDS0="cat ../data/$YYDY/bme680.dat.$YYDATE ../data/$YDY/bme680.dat.$YDATE ../data/$DY/bme680.dat.$DATE | grep -aP \"$PAT0\""
-TDS1="cat ../data/$YYDY/pi_temp.$YYDATE ../data/$YDY/pi_temp.$YDATE ../data/$DY/pi_temp.$DATE | grep -aP \"$PAT1\""
-TDS2="cat ../data/$YYDY/derived.dat.$YYDATE ../data/$YDY/derived.dat.$YDATE ../data/$DY/derived.dat.$DATE"
-
-TD_DUMP0="eval $TDS0"
-TD_DUMP1="eval $TDS1"
-TD_DUMP2="eval $TDS2"
-
-$TD_DUMP0 | grep -A $NSAMP $YYDATEH > $TD || $TD_DUMP0 > $TD
-$TD_DUMP1 | grep -A $NSAMP $YYDATEH > $TDPT || $TD_DUMP1 > $TDPT
-$TD_DUMP2 | grep -A $NSAMP $YYDATEH > $TDDD || $TD_DUMP2 > $TDDD
-
-gnuplot -e "TD='$TD';TDPT='$TDPT';TDDD='$TDDD'" $OVER_DIR/weather_specs.gnuplot
-
-# try not to loose 20 min of data from hard reboots
 sync
 
-rsync -e "ssh -q" --timeout=60 -ur $OVER_DIR/* wx1@dunkledaten.de:/wx1/ 2>/dev/null
+/usr/bin/rsync -e "ssh -q" --timeout=60 -ur $WT_DIR/* wx1_sync:/wx1/ 2>/dev/null
 
 rm $LOCK
