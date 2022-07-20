90b39001b6418b74d7c1c077fff961f280442f2a
mcthoren <thoren.mcdole@gmail.com>
2022-07-20 16:52:48 +0000

    add wet-bulb temp to html table

--- sensor_data_grab.py
+++ sensor_data_grab.py
@@ -8,7 +8,7 @@ import wxlib as wx
 
 wx_dir = "/home/ghz/wx"
 
-def gen_index(etemp, ehum, press, pitemp, edp, abs_hum, heat_i, title):
+def gen_index(etemp, ehum, press, pitemp, edp, abs_hum, heat_i, Tw, title):
 	plate = wx_dir+"/wx_index.html.template"
 	plate_fd = open(plate, 'r')
 	plate_dat = plate_fd.read()
@@ -23,6 +23,7 @@ def gen_index(etemp, ehum, press, pitemp, edp, abs_hum, heat_i, title):
 	plate_dat = plate_dat.replace("PITEMP", str("%.2f" % pitemp))
 	plate_dat = plate_dat.replace("ABSHUM", str("%.2f" % abs_hum))
 	plate_dat = plate_dat.replace("HTIDX", str("%.2f" % heat_i))
+	plate_dat = plate_dat.replace("WBT", str("%.2f" % Tw))
 	plate_dat = plate_dat.replace("DATE", ts)
 	plate_dat = plate_dat.replace("TITLE", title)
 
@@ -69,4 +70,4 @@ if __name__ == "__main__":
 	% (ts, abs_hum, heat_i, Tdew, Tw)
 	wx.write_out_dat_stamp(ts, 'derived.dat', derived_dat_string, wx_dir)
 
-	gen_index(e_temp, e_hum, press + press_cal, float(pi_temp) / 1000, Tdew, abs_hum, heat_i, title)
+	gen_index(e_temp, e_hum, press + press_cal, float(pi_temp) / 1000, Tdew, abs_hum, heat_i, Tw, title)
